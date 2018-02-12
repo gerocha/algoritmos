aea970460b01465ce78d6ca98ec0d762e0cf538c
Gel <gel@leandro@alertaapp.com.br>
2018-02-12 20:53:10 -0200

    criada funcao insertion_sort aula 2.1

---
+++ 2/insertion-sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(a):
+    for j in range(1,len(a)):
+        chave = a[j]
+        i = j - 1
+        print chave
+        print i
+        #quando -1 entao preencheu o primeiro valor a[0]
+        while i > -1 and a[i] > chave:
+            a[i + 1] = a[i]
+            i = i - 1
+        a[i+1] = chave
+        print a
+    return a
+
+insertion_sort([5,6,3,13,1])
