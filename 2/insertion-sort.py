55b6895e370ad555a91cf79d2bd5022225f8e9c1
Gel <gel@leandro@alertaapp.com.br>
2018-02-12 21:41:57 -0200

    resolvendo algumas questoes

--- 2/insertion-sort.py
+++ 2/insertion-sort.py
@@ -2,13 +2,12 @@ def insertion_sort(a):
     for j in range(1,len(a)):
         chave = a[j]
         i = j - 1
-        print chave
-        print i
         #quando -1 entao preencheu o primeiro valor a[0]
         while i > -1 and a[i] > chave:
             a[i + 1] = a[i]
             i = i - 1
         a[i+1] = chave
+        print i
         print a
     return a
 
